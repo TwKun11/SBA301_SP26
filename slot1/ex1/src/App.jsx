9d20241b04c414c1e83cc642ac1d7da99cee3907
TwKun11 <trikun2k4@gmail.com>
2026-01-06 10:34:32 +0700

    ex1

--- slot1/ex1/src/App.jsx
+++ slot1/ex1/src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 function App() {
   return (
-    <>
-      <div>Hi</div>
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <main className="flex-fill py-4">
+        <div className="container text-center">
+          <h1>WELCOME TO MY WEBSITE</h1>
+        </div>
+      </main>
+
       <Footer />
-    </>
+    </div>
   );
 }
 
