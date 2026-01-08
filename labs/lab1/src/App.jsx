d934c5697d8b2b5fee08459bcf5f31d3c7277c37
TwKun11 <trikun2k4@gmail.com>
2026-01-08 11:29:18 +0700

    lab2

--- labs/lab1/src/App.jsx
+++ labs/lab1/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Anh from "../public/images/4n.jpg";
 import ListOfOrchid from "./components/ListOfOrchid";
+import listOrchids from "./data/listOrchids";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <main className="flex-fill py-4">
         <div className="container">
           <h1 className="text-center mb-4">WELCOME TO MY WEBSITE</h1>
-          <ListOfOrchid />
+          <ListOfOrchid orchids={listOrchids} />
         </div>
       </main>
 
--- labs/lab1/src/components/ListOfOrchid.jsx
+++ labs/lab1/src/components/ListOfOrchid.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Orchid from "./Orchid";
-import listOrchids from "../data/listOrchids";
-function ListOrchid() {
+
+function ListOrchid({ orchids }) {
   return (
     <Container className="py-5">
       <Row>
-        {listOrchids.map((orchid) => (
+        {orchids.map((orchid) => (
           <Col md={6} lg={3} className="mb-4" key={orchid.id}>
             <Orchid {...orchid} />
           </Col>
